e9574aa88819d9977168be4cdd55e41012f15de3
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-17 11:07:55 +0200

    ..

--- backup-test.sh
+++ backup-test.sh
@@ -5,6 +5,6 @@ cd Fule-Butterfly-Backup
 git pull https://github.com/sutonagy/Fule-Butterfly-Backup.git
 #python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml -K 2304091311
 #python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml -K 2304131312
-#python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml -K 2304161313
-python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml
+python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml -K 2304161313
+#python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml
 #python3 -m pdb bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml
