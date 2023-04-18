90529db8751f2cd1aebb8c72b122ac5b8ce0b73c
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-18 17:03:05 +0200

    ..

--- backup-test.sh
+++ backup-test.sh
@@ -3,12 +3,12 @@ rm -rf /backup/rsync/
 mkdir -p /backup/rsync/log
 cd Fule-Butterfly-Backup
 git pull https://github.com/sutonagy/Fule-Butterfly-Backup.git
-python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml -K 2304011307
-python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml -K 2304021308
-python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml -K 2304031309
-python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml -K 2304041310
-python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml -K 2304091311
-python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml -K 2304131312
-python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml -K 2304161313
-python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml
-#python3 -m pdb bb.py -M /home/alma/Fule-Butterfly-Backup/default.yml
+python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.bck -K 2304011307
+python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.bck -K 2304021308
+python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.bck -K 2304031309
+python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.bck -K 2304041310
+python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.bck -K 2304091311
+python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.bck -K 2304131312
+python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.bck -K 2304161313
+python3 bb.py -M /home/alma/Fule-Butterfly-Backup/default.bck
+#python3 -m pdb bb.py -M /home/alma/Fule-Butterfly-Backup/default.bck
