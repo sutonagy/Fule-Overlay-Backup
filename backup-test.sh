7350f6596a3fc2183ff476cf4bccfd8bda3e8922
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-20 09:13:23 +0200

    ..

--- backup-test.sh
+++ backup-test.sh
@@ -3,12 +3,12 @@ rm -rf /backup/rsync/
 mkdir -p /backup/rsync/log
 cd Fule-Butterfly-Backup
 git pull https://github.com/sutonagy/Fule-Butterfly-Backup.git
-python3 bb.py -M /etc/bb/bb.yaml -K 2304011307
-python3 bb.py -M /etc/bb/bb.yaml -K 2304021308
-python3 bb.py -M /etc/bb/bb.yaml -K 2304031309
-python3 bb.py -M /etc/bb/bb.yaml -K 2304041310
-python3 bb.py -M /etc/bb/bb.yaml -K 2304091311
-python3 bb.py -M /etc/bb/bb.yaml -K 2304131312
-python3 bb.py -M /etc/bb/bb.yaml -K 2304161313
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304011307
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304021308
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304031309
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304041310
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304091311
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304131312
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304161313
 python3 bb.py -M /etc/bb/bb.yaml
 #python3 -m pdb bb.py -M /etc/bb/bb.yaml
