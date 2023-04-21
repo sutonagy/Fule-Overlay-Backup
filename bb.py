3f456452f90630a07e649ca8594a3efcce476327
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-21 14:17:43 +0200

    .

--- bb.py
+++ bb.py
@@ -323,7 +323,7 @@ def logger_init(loggername):
     logger.addHandler(ch)
     logger.addHandler(fh)
     #logger.info('loglevel: %s', args.loglevel)
-    return logger, fh, ch
+    return logger, fh, ch, logging
 
 # region Global Variables
 VERSION = 'v0.9.01'
@@ -1964,7 +1964,7 @@ if __name__ == '__main__':
     import traceback
     import sys
     import os
-    logger, fh, ch=logger_init('main')
+    logger, fh, ch, logging = logger_init('main')
     logger.info('Eleje')
     logger.info('Loglevel: {0}, console loglevel: {1}'.format(logger.getLevelName(fh.level), logger.getLevelName(ch.level)))
     print('Loglevel: {0}, console loglevel: {1}'.format(logger.getLevelName(fh.level), logger.getLevelName(ch.level)))
