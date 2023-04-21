3a67e42daed548eccd0ef595ae7e445764e22a35
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-21 12:39:37 +0200

    ...

--- bb.py
+++ bb.py
@@ -57,7 +57,8 @@ import argparse
 import configparser
 import os
 import subprocess
-import utility.PrintColor, utility.print_verbose, utility.check_tool, utility.write_log, utility.time_to_sting, utility.string_to_time, utility.touch, utility.cleanup, utility.archive, utility.time_for_log, utility.check_ssh, utility.check_rsync, utility.make_symlink, utility.confirm, utility.pager, utility.find_replace, utility.datetime_spec, utility.time_for_folder, utility.send_telegram_message
+import utility.PrintColor.PURPLE, utility.PrintColor.CYAN, utility.PrintColor.DARKCYAN, utility.PrintColor.BLUE, utility.PrintColor.GREEN, utility.PrintColor.YELLOW, utility.PrintColor.RED, utility.PrintColor.BOLD, utility.PrintColor.UNDERLINE, utility.PrintColor.END
+import utility.print_verbose, utility.check_tool, utility.write_log, utility.time_to_sting, utility.string_to_time, utility.touch, utility.cleanup, utility.archive, utility.time_for_log, utility.check_ssh, utility.check_rsync, utility.make_symlink, utility.confirm, utility.pager, utility.find_replace, utility.datetime_spec, utility.time_for_folder, utility.send_telegram_message
 import time
 import yaml
 import types
