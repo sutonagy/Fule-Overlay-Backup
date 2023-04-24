bb10555f46d898ec891286b95e7ca87b13c45b0d
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-24 13:56:36 +0200

    ..

--- bb.py
+++ bb.py
@@ -1283,7 +1283,7 @@ def check_configuration(ip):
             return False
     # except subprocess.CalledProcessError:
     except Exception as e:
-        logger.error('ERROR: {0}'format(e.output.decode())) # print out the stdout messages up to the exception
+        logger.error('ERROR: {0}'.format(e.output.decode())) # print out the stdout messages up to the exception
         logger.error('ERROR: {0}'.format(e)) # To print out the exception message
         logger.error('ERROR: SSH key configuration error.') # To print out the exception message
         return False
