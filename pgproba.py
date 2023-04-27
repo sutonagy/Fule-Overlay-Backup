9bdee2e8293c1084c943f9be198c093623f8135a
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-27 17:41:13 +0200

    ...

---
+++ pgproba.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import gzip
+from plumbum.cmd import pg_dump
+with gzip.open(‘backup.gz’, ‘wb’) as f:
+   (pg_dump[“-h”, “localhost”, “-U”, “postgres”, “Arpad48-50”] > f)
+()
