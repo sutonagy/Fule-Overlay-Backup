6c16a8d71ab5821486100d785c5682ecd4c2dae9
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-27 18:42:50 +0200

    ..

--- pgproba.py
+++ pgproba.py
@@ -2,6 +2,7 @@
 
 import gzip
 from plumbum.cmd import pg_dump
-with gzip.open('backup.gz', 'wb') as f:
-   (pg_dump["-h", "localhost", "-U", "postgres", "postgres"] )
+with open(w'backup.sql') as f:
+#with gzip.open('backup.gz', 'wb') as f:
+   (pg_dump["-h", "localhost", "-U", "postgres", "postgres"] > f)
 ()
