97aba5ca4157ad93474f9daf6ffef70068eaca90
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-27 18:53:52 +0200

    ...

--- pgproba.py
+++ pgproba.py
@@ -2,11 +2,6 @@
 
 import gzip
 from plumbum.cmd import pg_dump
-#with open('backup.sql','w') as f:
 with gzip.open('backup.gz', 'wb') as f:
-    # \n is placed to indicate EOL (End of Line)
-    L = ["This is Delhi \n","This is Paris \n","This is London \n"]
-    f.write("Hello \n")
-    f.writelines(L)
     (pg_dump["-h", "localhost", "-U", "postgres", "postgres"] > f)  
 ()
