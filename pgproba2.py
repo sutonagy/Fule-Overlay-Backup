d22248b6db8c0ad1e77096eb7fe86f2ca08735dc
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-28 10:36:58 +0200

    ....

--- pgproba2.py
+++ pgproba2.py
@@ -9,7 +9,7 @@ keyfile = "/etc/bb/sshkeys/rbackup.oss"
 command = "ls"
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(host, port, username, keyfile)
+ssh.connect(host, port=port, username=username, key_filename=keyfile)
 stdin, stdout, stderr = ssh.exec_command(command)
 lines = stdout.readlines()
 print(lines)
