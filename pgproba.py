eccf219f06a3d356488f13a583a8696596daf9dc
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-28 09:19:28 +0200

    ..

--- pgproba.py
+++ pgproba.py
@@ -3,6 +3,7 @@
 import gzip
 from plumbum import SshMachine
 remote = SshMachine("mail2022.platinum.co.hu", user = "rbackup", keyfile = "/etc/bb/sshkeys/rbackup.oss", ssh_opts = ["-o", "StrictHostKeyChecking=no"])
+print('Connected to mail2022.platinum.co.hu')
 with gzip.open('backup.gz', 'wb') as f:
     #(ssh["rbackup@mail2022.platinum.co.hu", "-o", "StrictHostKeyChecking=no", "-i", "/etc/bb/sshkeys/rbackup.oss"] > f)
     remote['pg_dump']("-h", "localhost", "-U", "postgres", "postgres") > f
