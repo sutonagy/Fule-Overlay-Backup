015e8035889cb99919ab553c277c8caf690328c6
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-28 10:08:34 +0200

    ..

---
+++ pgproba2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import paramiko
+host = "mail2022.platinum.co.hu"
+port = 22
+username = "rbackup"
+# password = "password"
+keyfile = "/etc/bb/sshkeys/rbackup.oss"
+command = "ls"
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(host, port, username, keyfile)
+stdin, stdout, stderr = ssh.exec_command(command)
+lines = stdout.readlines()
+print(lines)
+
+import gzip
+from plumbum import SshMachine
+remote = SshMachine("mail2022.platinum.co.hu", user = "rbackup", keyfile = "/etc/bb/sshkeys/rbackup.oss", ssh_opts = ["-o", "StrictHostKeyChecking=no"])
+print('Connected to mail2022.platinum.co.hu')
+with open('backup.sql', 'w') as f:
+#with gzip.open('backup.gz', 'wb') as f:
+    #(ssh["rbackup@mail2022.platinum.co.hu", "-o", "StrictHostKeyChecking=no", "-i", "/etc/bb/sshkeys/rbackup.oss"] > f)
+    rm_pgd = remote['pg_dump']("-h", "localhost", "-U", "postgres", "postgres")
+    f.write(rm_pgd())
+#
