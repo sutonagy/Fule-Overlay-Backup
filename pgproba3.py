450877fae86562fe85c23451738c751b9b3bb5bd
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-28 12:26:13 +0200

    ...

---
+++ pgproba3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import asyncio, asyncssh, sys
+
+async def run_client():
+    conn = await asyncio.wait_for(asyncssh.connect('mail2022.platinum.co.hu', username='rbackup', client_keys=['/etc/bb/sshkeys/rbackup.oss'], known_hosts = None,
+                                                    keepalive_interval=600, keepalive_count_max=10000),10,)
+
+    return conn
+
+async def run_command():    
+    try:
+        conn = await run_client()        
+        result = await conn.run('ls')
+
+        if result.exit_status == 0:            
+            print(result.stdout, end='')                        
+        else:
+            print(result.stderr, end='', file=sys.stderr)
+            print('Program exited with status %d' % result.exit_status,
+                file=sys.stderr)
+    except Exception as ex:
+        print(ex)      
+
+async def program():
+    # Run both print method and wait for them to complete (passing in asyncState)    
+    await asyncio.gather(run_command())
+
+# Run our program until it is complete
+loop = asyncio.get_event_loop()
+loop.run_until_complete(program())
+loop.close()
