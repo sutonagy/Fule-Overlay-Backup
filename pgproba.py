983bd248071c592161965b0d619fdb8c2fa385b2
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-28 09:16:22 +0200

    ...

--- pgproba.py
+++ pgproba.py
@@ -6,4 +6,4 @@ remote = SshMachine("mail2022.platinum.co.hu", user = "rbackup", keyfile = "/etc
 with gzip.open('backup.gz', 'wb') as f:
     #(ssh["rbackup@mail2022.platinum.co.hu", "-o", "StrictHostKeyChecking=no", "-i", "/etc/bb/sshkeys/rbackup.oss"] > f)
     remote['pg_dump']("-h", "localhost", "-U", "postgres", "postgres") > f
-()
+#
