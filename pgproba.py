1a7622824d4f405608b4997a610458cae0ebef8c
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-28 09:25:46 +0200

    ....

--- pgproba.py
+++ pgproba.py
@@ -7,5 +7,5 @@ print('Connected to mail2022.platinum.co.hu')
 with open('backup.sql', 'w') as f:
 #with gzip.open('backup.gz', 'wb') as f:
     #(ssh["rbackup@mail2022.platinum.co.hu", "-o", "StrictHostKeyChecking=no", "-i", "/etc/bb/sshkeys/rbackup.oss"] > f)
-    remote['pg_dump']("-h", "localhost", "-U", "postgres", "postgres") > f
+    remote['pg_dump']("-h", "localhost", "-U", "postgres", "postgres")
 #
