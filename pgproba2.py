7effdf2d59bc85ae4bbf958ec70ac717415cef9d
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-04-28 10:31:47 +0200

    ...

--- pgproba2.py
+++ pgproba2.py
@@ -13,14 +13,4 @@ ssh.connect(host, port, username, keyfile)
 stdin, stdout, stderr = ssh.exec_command(command)
 lines = stdout.readlines()
 print(lines)
-
-import gzip
-from plumbum import SshMachine
-remote = SshMachine("mail2022.platinum.co.hu", user = "rbackup", keyfile = "/etc/bb/sshkeys/rbackup.oss", ssh_opts = ["-o", "StrictHostKeyChecking=no"])
-print('Connected to mail2022.platinum.co.hu')
-with open('backup.sql', 'w') as f:
-#with gzip.open('backup.gz', 'wb') as f:
-    #(ssh["rbackup@mail2022.platinum.co.hu", "-o", "StrictHostKeyChecking=no", "-i", "/etc/bb/sshkeys/rbackup.oss"] > f)
-    rm_pgd = remote['pg_dump']("-h", "localhost", "-U", "postgres", "postgres")
-    f.write(rm_pgd())
-#
+ssh.close()
