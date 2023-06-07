71622447152a1d4ccf77ab0d9bc31602a9deb0cd
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-06-07 12:56:22 +0200

    ...

--- dbdump.py
+++ dbdump.py
@@ -13,7 +13,8 @@ def dbdump_async(args,configfile=None):
         try:
             conn = await asyncio.wait_for(asyncssh.connect(host, username='rbackup', client_keys=['/etc/bb/sshkeys/rbackup.oss'], known_hosts = None,
                                                         keepalive_interval=600, keepalive_count_max=10000), timeout=12)
-        except (OSError, asyncssh.Error) as exc:
+        #except (OSError, asyncssh.Error) as exc:
+        except Exception as exc:
             sys.exit('SSH connection failed: ' + str(exc))
         return conn
 
