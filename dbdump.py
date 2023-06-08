3680020ab90ee7a475547da0e44d45ae1a3e430d
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-06-08 17:26:18 +0200

    ...

--- dbdump.py
+++ dbdump.py
@@ -124,9 +124,8 @@ def dbdump_async(args,configfile=None):
             print(host,database,dtype)
             async with await run_client(host) as conn:
                 if dtype == 'mysql':
-                    sqlcommand = "select count(*) AS tnum from information_schema.tables where table_schema = '%s';" % database
-                    tables_number = await conn.run("mysql -h %s --user=%s --password=%s --port=%s -N -e '%s';' | grep -E '[a-z0..9]'" % (server, user, password, port, sqlcommand), check=True)
-                    print(tables_number.stdout)
+                    tables_number = await conn.run("mysql -h %s --user=%s --password=%s --port=%s -N -e \"select count(*) AS tnum from information_schema.tables where table_schema = '%s';\" | grep -E '[a-z0..9]'" % (server, user, password, port, database), check=True)
+                    print(tables_number.stdout, tables_number.stderr, tables_number.exit_status)
                     if tables_number.stdout != 0:
                         tables = await conn.run("mysql -h %s --user=%s --password=%s --port=%s -N -e 'show tables;' %s | grep -E '[a-z]'" % (server, user, password, port, database), check=True)
                     else:
