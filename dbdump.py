5ae07349f3da123a0b381a7ca004725f0a35edb0
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-06-09 13:20:34 +0200

    ...

--- dbdump.py
+++ dbdump.py
@@ -154,19 +154,21 @@ def dbdump_async(args,configfile=None):
                 exclude = False
                 for exclude_database in exclude_databases:
                     if re.search(exclude_database, database):
-                        print('Exclude database pattern matched: %s, Database: %s' % (exclude_database,database))
+                        #print('Exclude database pattern matched: %s, Database: %s' % (exclude_database,database))
                         exclude = True
                         break
                     else:
-                        print('Exclude database pattern not matched: %s, Database: %s' % (exclude_database,database))
+                        pass
+                        #print('Exclude database pattern not matched: %s, Database: %s' % (exclude_database,database))
                 if not exclude:
                     for include_database in include_databases:
                         if re.search(include_database, database):
-                            print('Include database pattern matched: %s, Database: %s' % (include_database,database))
+                            #print('Include database pattern matched: %s, Database: %s' % (include_database,database))
                             runtask = True
                             break
                         else:
-                            print('Include database pattern not matched: %s, Database: %s' % (include_database,database))
+                            pass
+                            #print('Include database pattern not matched: %s, Database: %s' % (include_database,database))
                 print('Database: %s is %s' % (database,runtask))
                 if runtask:
                     try:
