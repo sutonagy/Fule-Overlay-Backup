2c69fbfc381627196a45e79def494f4fcc32b2ec
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-06-11 17:54:23 +0200

    ...

--- backup-test.sh
+++ backup-test.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 rm -rf /backup/rsync/
+rm -rf /backup/data/
+rm -rf /backup/dumperror/
 mkdir -p /backup/rsync/log
 cd Fule-Butterfly-Backup
 git pull https://github.com/sutonagy/Fule-Butterfly-Backup.git
 start=`date +%s`
-python3 bb.py -M /etc/bb/bb.yaml -K 2304011307
-python3 bb.py -M /etc/bb/bb.yaml -K 2304021308
-python3 bb.py -M /etc/bb/bb.yaml -K 2304031309
-python3 bb.py -M /etc/bb/bb.yaml -K 2304041310
-python3 bb.py -M /etc/bb/bb.yaml -K 2304091311
-python3 bb.py -M /etc/bb/bb.yaml -K 2304131312
-python3 bb.py -M /etc/bb/bb.yaml -K 2304161313
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304011307
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304021308
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304031309
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304041310
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304091311
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304131312
+#python3 bb.py -M /etc/bb/bb.yaml -K 2304161313
 python3 bb.py -M /etc/bb/bb.yaml
 end=`date +%s`
 runtime=$((end-start))
