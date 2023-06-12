c11c61dc1e2a0f8487f0f8da4fa06545a20952d2
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-06-12 16:38:36 +0200

    ..

--- dbdump.py
+++ dbdump.py
@@ -309,7 +309,7 @@ def dbdump_async(args,configfile=None):
             #print(tasks)
             #print(75*'-')
             #results = await asyncio.gather(*tasks, return_exceptions=True)
-            results = await tqdm.asyncio.tqdm_asyncio.gather(*tasks, colour="white", bar_format="Eta:{eta}.|{bar}{r_bar}", desc="The progress of dumps", return_exceptions=True)
+            results = await tqdm.asyncio.tqdm_asyncio.gather(*tasks, colour="white", bar_format="Eta:{eta}.|{bar}{r_bar}", desc="The progress of dumps")
             #aktresults = results
             #print(aktresults)
 
