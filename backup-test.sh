40c28ff0e68c69a2ef19905e89d27477d7496b60
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-06-12 10:57:33 +0200

    ..

--- backup-test.sh
+++ backup-test.sh
@@ -3,6 +3,7 @@ rm -rf /backup/rsync/
 rm -rf /backup/data/
 rm -rf /backup/dumperror/
 mkdir -p /backup/rsynclog
+mkdir -p /backup/rsync
 cd Fule-Butterfly-Backup
 git pull https://github.com/sutonagy/Fule-Butterfly-Backup.git
 start=`date +%s`
