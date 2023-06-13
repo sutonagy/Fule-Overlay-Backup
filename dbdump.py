938426c37c3e71abdd0753f4651da7842a80fbc4
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-06-13 08:54:04 +0200

    ...

--- dbdump.py
+++ dbdump.py
@@ -112,7 +112,7 @@ def dbdump_async(args,configfile=None,serial=1):
                 for dumpcommand, mode in zip(dumpcommands, modes):
                     if database is None:
                         database = 'all'
-                    sqlpath='%s/%s/%s/%s/%s/%s' % (args.dumpdestination,host,dbtype,server,port/database)
+                    sqlpath='%s/%s/%s/%s/%s/%s' % (args.dumpdestination,host,dbtype,server,port,database)
                     if not os.path.exists(sqlpath): os.makedirs(sqlpath)
                     #print(dumpcommand, mode)
                     result = await conn.run(dumpcommand, stdout='%s/%s.sql' % (sqlpath,mode), stderr='%s/%s-%s-%s-%s-%s-%s.err' % (errpath,host,dbtype,server,port,database,mode), check=True)
