3347dc878701b60112fba8b72477142b2e31d175
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-06-14 11:40:16 +0200

    full teszt

--- backup-test.sh
+++ backup-test.sh
@@ -14,9 +14,22 @@ start=`date +%s`
 #python3 bb.py -M /etc/bb/bb.yaml -K 2304041310
 #python3 bb.py -M /etc/bb/bb.yaml -K 2304091311
 #python3 bb.py -M /etc/bb/bb.yaml -K 2304131312
-#python3 bb.py -M /etc/bb/bb.yaml -K 2304161313
+python3 bb.py -M /etc/bb/bb.yaml -K 2306131313
+end=`date +%s`
+runtime=$((end-start))
+echo "Total runtime of first backup: $runtime seconds"
+while true; do
+    echo "Full backup lefutott. Átmásoltad a korábbi backupot és módosítottad a confokat? Ha igen, akkor nyomj egy I-t, ha nem, akkor egy N-t"
+    read -rep "Mehet a következő backup? (I/N)" yn
+    case $yn in
+        [Ii]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Válaszolj igennel (I vagy i) vagy nemmel (N vagy n).";;
+    esac
+done
+start=`date +%s`
 python3 bb.py -M /etc/bb/bb.yaml
 end=`date +%s`
 runtime=$((end-start))
-echo "Total runtime: $runtime seconds"
+echo "Total runtimeof second backup: $runtime seconds"
 #python3 -m pdb bb.py -M /etc/bb/bb.yaml
