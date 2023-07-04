41c05b889fa283041dd38a725aec8cc6ec3ff1cc
Laszlo Suto Nagy <130842339+sutonagy@users.noreply.github.com>
2023-07-04 18:31:42 +0200

    ..

--- pgproba6.py
+++ pgproba6.py
@@ -2,4 +2,5 @@
 import requests
 
 response = requests.get("https://api.github.com/repos/sutonagy/Fule-Overlay-Backup/releases/latest")
-print(response.json()["name"])
+#print(response.json()["name"])
+print(response.json())
